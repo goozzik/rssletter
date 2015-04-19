850e495bb65b786d3cbde6f565ed9cb95b29bf01
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2015-04-19 17:22:10 +0200

    Add RSSItem model

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 require_all File.expand_path('../../lib/*.rb', __FILE__)
+require_all File.expand_path('../../lib/models/*.rb', __FILE__)
 
 class Rssletter < Sinatra::Base
   register Sinatra::ActiveRecordExtension
---
+++ lib/models/rss_item.rb
@@ -0,0 +1,3 @@
+class RSSItem < ActiveRecord::Base
+
+end
