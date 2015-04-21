78c79702c926fac82645e29133d18a07fda39c94
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2015-04-21 19:00:19 +0200

    config/application.rb: Load config yml

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,31 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
-require_all File.expand_path('../../lib/*.rb', __FILE__)
+require_all File.expand_path('../../lib/services/*.rb', __FILE__)
 require_all File.expand_path('../../lib/models/*.rb', __FILE__)
 
 class Rssletter < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+
+  def self.env
+    @@env ||= ENV['RACK_ENV'] || 'development'
+  end
+
+  def self.config
+    @@config ||= YAML.load(
+      ERB.new(
+        File.read(
+          File.expand_path('../application.yml', __FILE__)
+        )
+      ).result
+    )[env]
+  end
+
+end
+
+Mail.defaults do
+  retriever_method(
+    Rssletter.config[:mail][:retriever_method].to_sym,
+    Rssletter.config[:mail].except(:retriever_method)
+  )
 end
