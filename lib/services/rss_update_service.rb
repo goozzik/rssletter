a33c1618dd380d763cc4199146ffafccf22e4f94
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2015-04-23 19:26:52 +0200

    RSSUpdateService#create_rss_item: get content from mail

--- lib/services/rss_update_service.rb
+++ lib/services/rss_update_service.rb
@@ -28,11 +28,34 @@ class RSSUpdateService
   end
 
   def mail_already_coverted?(newsletter, mail)
-    newsletter.rss_items.where(subject: mail.subject).exists?
+    newsletter.rss_items.where(title: mail.subject).exists?
   end
 
   def create_rss_item(newsletter, mail)
-    newsletter.rss_items.create
+    newsletter.rss_items.create(
+      title: mail.subject, content: get_content_from_mail(mail)
+    )
+  end
+
+  def get_content_from_mail(mail)
+    content = ''
+
+    if mail.body.parts.blank?
+      content = mail.body.to_s
+    else
+      mail.body.parts.each do |part|
+        if mail_part_is_html?(part.content_type)
+          content = part.body.to_s
+          break
+        end
+      end
+    end
+
+    content.force_encoding('utf-8')
+  end
+
+  def mail_part_is_html?(part_content_type)
+    !!part_content_type.match(/^text\/html/)
   end
 
 end
--- spec/lib/services/rss_update_service_spec.rb
+++ spec/lib/services/rss_update_service_spec.rb
@@ -15,18 +15,26 @@ describe RSSUpdateService do
       end
 
       context 'and it comes from newsletter' do
+        let(:mail_body) do
+          instance_double(
+            'Mail::Body',
+            parts: [],
+            raw_source: 'Something really interesting.'
+          )
+        end
         let(:mail) do
           instance_double(
             'Mail::Message',
             from: [newsletter.email],
-            subject: 'Check out new buttons!'
+            subject: 'Check out new buttons!',
+            body: mail_body,
           )
         end
 
         context 'and this mail was already converted to RSS item' do
           let!(:rss_item) do
             FactoryGirl.create(
-              :rss_item, newsletter: newsletter, subject: mail.subject
+              :rss_item, newsletter: newsletter, title: mail.subject
             )
           end
 
@@ -42,6 +50,49 @@ describe RSSUpdateService do
             expect { subject.update }.to change {
               newsletter.rss_items.count
             }.by(1)
+
+            rss_item = newsletter.rss_items.last
+            expect(rss_item.title).to eq(mail.subject)
+          end
+
+          context 'and there is only one format of mail body' do
+            it 'assigns raw source of mail body as RSS item content' do
+              subject.update
+
+              rss_item = newsletter.rss_items.last
+              expect(rss_item.content).to eq(mail.body.raw_source)
+            end
+          end
+
+          context 'and there are text/plain and text/html body formats' do
+            let(:mail_html_body_part) do
+              instance_double(
+                'Mail::Part',
+                content_type: "text/html; charset=ascii",
+                body: instance_double('Mail::Body', raw_source: '<html></html>')
+              )
+            end
+            let(:mail_plain_body_part) do
+              instance_double(
+                'Mail::Part',
+                content_type: "text/plain; charset=ascii",
+                body: instance_double('Mail::Body', raw_source: 'raw')
+              )
+            end
+            let(:mail_body_parts) do
+              [mail_html_body_part, mail_plain_body_part]
+            end
+
+            before do
+              allow(mail_body).to receive(:parts).and_return(mail_body_parts) 
+            end
+
+            it 'assigns text/html format of mail body as RSS item content' do
+              subject.update
+
+              rss_item = newsletter.rss_items.last
+              expect(rss_item.content).to eq(mail_html_body_part.body.raw_source)
+            end
           end
         end
       end
