fe8b647dfc2c469f340058c6622382284e661d44
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2015-04-26 10:27:22 +0200

    Add index page for newsletters

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require File.expand_path("../config/application", __FILE__)
 
 class Rssletter
 
+  set :haml, :format => :html5
+  set :views, settings.root + '/../views'
+
   get '/newsletter/:id' do
     begin
       newsletter.to_rss
@@ -11,10 +14,18 @@ class Rssletter
     end
   end
 
+  get '/newsletters' do
+    haml :index, locals: { newsletters: newsletters }, views: "#{settings.views}/newsletters/"
+  end
+
   private
 
   def newsletter
     @newsletter ||= Newsletter.find(params[:id])
+  end
+
+  def newsletters
+    @newsletters ||= Newsletter.all
   end
 
 end
